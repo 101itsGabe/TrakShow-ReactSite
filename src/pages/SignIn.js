681d5908cf659af4358db1828163e3ce6a845903
101itsGabe <manngabe@gmail.com>
2024-07-10 13:16:16 -0500

    redirects and sign in page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,36 +2,35 @@ import React, { useState, useEffect } from "react";
 import { googleSignIn, getUserShows, emailSignIn } from "../api/FirebaseApi";
 import { Text, View } from "react";
 import googleimg from "../images/googlelogo.jpg";
-import { useNavigate,Navigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
+import logo from "../images/logo.png";
 
 export const SignIn = ({ setCurUser, setUserShow }) => {
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const nav = useNavigate();
 
-    const [username, setUserName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const nav = useNavigate();
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handleEmail = (e) => {
-      setEmail(e.target.value);
-    }
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handlePassword = (e) =>{
-      setPassword(e.target.value);
-    }
-
-    const handleSignIn = async() =>{
-      try{
+  const handleSignIn = async () => {
+    try {
       const curUser = await emailSignIn(email, password);
       setCurUser(curUser);
       //console.log(curUser);
       const username = curUser.email.split("@")[0];
       const shows = await getUserShows(username);
       setUserShow(shows);
-      }
-      catch(error){
-        console.log(error.message);
-      }
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
   const btnSignIn = async () => {
     try {
@@ -46,44 +45,65 @@ export const SignIn = ({ setCurUser, setUserShow }) => {
     }
   };
 
-  const navToSignUp = () =>{
+  const navToSignUp = () => {
     nav("/signuppage");
-  }
+  };
 
   return (
-    <div>
-      <p style={{
-        fontSize: 32,
-        color: "white",
-        fontWeight: "bold",
-      }}>Welcome!</p>
+    <div className="sign-in">
+      <img src={logo} style={{padding: 30}}></img>
+      <p
+        style={{
+          fontSize: 32,
+          color: "white",
+          fontWeight: "bold",
+        }}
+      >
+        Welcome!
+      </p>
       <div className="sign-in-input">
-        <div style={{ display: "inline-block" }}>
-          <p style={{color: "white"}}>Email</p>
-          <input type="text" value={email} onChange={(e) =>{
-            handleEmail(e)
-          }} />
+        <div>
+          <p style={{ color: "white", fontWeight: "bold" }}>Email</p>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => {
+              handleEmail(e);
+            }}
+          />
         </div>
-        <div style={{ display: "inline-block" }}>
-          <p style={{color: "white"}}>Password</p>
-          <input type="password" value={password} onChange={(e) => {
-            handlePassword(e);
-          }}/>
+        <div>
+          <p style={{ color: "white", fontWeight: "bold" }}>Password</p>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              handlePassword(e);
+            }}
+          />
         </div>
       </div>
-      <button className="Google-Btn" style={{paddin: 10}} onClick={handleSignIn}> Sign In</button>
-      <div
-      style={{padding:20}}>
-        <button 
+      <button
         className="Google-Btn"
-        style={{padding: 10}}
-        onClick={btnSignIn}>
-            <img src={googleimg} width={30} height={20} />
-            Sign In</button>
+        style={{ paddin: 10 }}
+        onClick={handleSignIn}
+      >
+        {" "}
+        Sign In
+      </button>
+      <div style={{ padding: 20 }}>
+        <button
+          className="Google-Btn"
+          style={{ padding: 10 }}
+          onClick={btnSignIn}
+        >
+          <img src={googleimg} width={40} height={25} style={{marginRight: 10, borderRadius: 10}} />
+          Sign In
+        </button>
       </div>
-      <button 
-            className="Google-Btn"
-            onClick={navToSignUp}>Sign Up</button>
+      <button className="Google-Btn" onClick={navToSignUp}>
+        Sign Up
+      </button>
     </div>
   );
 };
